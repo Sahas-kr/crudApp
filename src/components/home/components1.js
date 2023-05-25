9708ad1112fd8598bc05058a7495ab79c6bb6518
unknown <sahasiyer23@gmail.com>
2023-05-25 20:47:57 +0530

    first commit

--- src/components/home/components1.js
+++ src/components/home/components1.js
@@ -1,22 +1,25 @@
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Components1() {
   return (
     <div>
+      
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand text-success " style={{ marginLeft: "50px" }} href="/">React
-            <i class="fa-brands fa-react fa-spin fa-lg mx-1"></i></a>
+          <Link class="navbar-brand text-success " style={{ marginLeft: "50px" }} to="/">React
+            <i class="fa-brands fa-react fa-spin fa-lg mx-1"></i></Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto mx-5">
               <li class="nav-item">
-                <a class="nav-link text-primary" id='bg' href="/Createstudent">Create Student</a>
+                <Link class="nav-link text-primary" id='bg' to="/Createstudent">Create Student</Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-primary" id='bg' href="/Studentlist">Student List</a>
+                <Link class="nav-link text-primary" id='bg' to="/Studentlist">Student List</Link>
               </li>
             </ul>
           </div>
